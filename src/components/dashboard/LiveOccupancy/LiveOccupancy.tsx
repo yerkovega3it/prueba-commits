29c6a4e2fe6d80d7cad1a39c13c63a23926a806f
jorgechirinos3it <jorge.chirinos@3it.cl>
2025-12-17 18:04:17 -0300

    chore: add missing stat card

--- src/components/dashboard/LiveOccupancy/LiveOccupancy.tsx
+++ src/components/dashboard/LiveOccupancy/LiveOccupancy.tsx
@@ -4,6 +4,7 @@ import StatCard from "../shared/StatCard";
 import {
   faUtensils,
   faPlaneDeparture,
+  faArrowRight,
 } from "@fortawesome/free-solid-svg-icons";
 
 function LiveOccupancy() {
@@ -11,13 +12,14 @@ function LiveOccupancy() {
     peopleOnSite,
     peopleRepeatedSameDiningHallConsumption,
     peopleDidNotShowUpForFlight,
+    peopleOutOfShiftAndNotRegisteredExit,
   } = useLaborStatus();
 
   return (
     <DashboardCard
       title="ESTADO EN FAENA"
       titleClassName="text-3xl font-bold whitespace-nowrap text-approved"
-      contentClassName="flex flex-col items-center gap-24 lg:gap-20 xl:gap-12 mt-20 lg:mt-16 xl:mt-12 h-full"
+      contentClassName="flex flex-col items-center gap-24 lg:gap-14 xl:gap-12 mt-20 lg:mt-16 xl:mt-12 h-full"
       className="col-span-1 p-4 rounded-3xl bg-main h-full"
     >
       <div className="w-full mx-auto flex-none flex flex-col items-center">
@@ -28,7 +30,12 @@ function LiveOccupancy() {
           Personas en Faena
         </p>
       </div>
-      <div className="rounded-3xl bg-main flex-none flex flex-col items-stretch justify-center gap-16 lg:gap-14 w-full mx-auto">
+      <div className="rounded-3xl bg-main flex-none flex flex-col items-stretch justify-center gap-6 lg:gap-8 w-full mx-auto mb-2">
+        <StatCard
+          icon={faArrowRight}
+          value={peopleOutOfShiftAndNotRegisteredExit}
+          label="Personas fuera de turno y no han marcado salida"
+        />
         <StatCard
           icon={faUtensils}
           value={peopleRepeatedSameDiningHallConsumption}
